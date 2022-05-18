592bb81706e5100953a3cf36205ab5079d30e421
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-05-18 10:50:29 -0500

    fixed questions three

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -34,11 +34,11 @@ console.log('--- 3. Accessing items in an array ---');
 console.log('First animal is', animalArray[0])
 
 // 3.a. TODO: Log the second animal in the array 
-console.log('First favorite food is', favFoods[0])
+console.log('Second animal is', animalArray[1])
 
 
 // 3.b. TODO: Log the last animal in the array using it's array index 
-console.log('Last favorite good is', favFoods[4])
+console.log('Last animal is', animalArray[3])
 
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length, 
